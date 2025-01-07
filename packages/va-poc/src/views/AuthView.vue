e48ceb2a90a0c6782d111a53567e7396a30c9115
Maarten Duhoux <maarten.duhoux@era.europa.eu>
2025-01-07 09:13:29 +0100

    (debug) update to AuthView

--- packages/va-poc/src/views/AuthView.vue
+++ packages/va-poc/src/views/AuthView.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
 
-import { inject } from 'vue';
+import { computed, inject, watchEffect } from 'vue';
 import { I18nInjectionKey } from 'vue-i18n';
 
 // As a component, translation is done using the main ts plugin
 // OPTION 1 - As a component, translation is done using the plugin
 import { useI18n } from "vue-i18n";
 import { i18nStore } from '../components/providers/i18nHost';
-// const props = defineProps<{ loc: string, justAProp:number }>(); FAILS for prop loc!
 const { t, locale } = useI18n();
-// locale.value = props.loc; FAILS
-locale.value = i18nStore.selectedLocale; // TEST
-
-// OPTION 2 FAILS - Direct import does not work in normal components
-// const i18n = inject(I18nInjectionKey);
-// const $t = i18n!.global.t;
-// const locale = i18n!.global.locale;
+const newLocale = computed(() => i18nStore.selectedLocale);
+watchEffect(() => {
+  console.log(`Language captured in HomeView watcher, changed to ${newLocale.value}!`);
+  locale.value = newLocale.value;
+});
 
 </script>
 
